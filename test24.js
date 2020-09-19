092659debe1dfa31d1f2a271cff7307d905ad107
mhemeedpro <mosabmhemed@gmail.com>
2020-09-19 18:35:22 +0300

    adding 2 new file to bots dir its bemo.js & sawa.js

--- test24.js
+++ test24.js
@@ -1,7 +1,11 @@
 const puppeteer = require('puppeteer');
+//link login to web site
 const url = "https://www.bbsfonline.com/BbsfOnline/Public/User/Login"
-const UserName ="mosaabmosaab"
-const password="123456789"
+//const verables user data 
+const user ="mosaabmosaab"
+const pass="1234567890"
+const phoneNumber=""
+var typebill=1 // is mean phone number
 if (!url) {
     throw "Please provide URL as a first argument";
 }
@@ -12,13 +16,55 @@ async function run () {
     await page.evaluate(() => {
       //  document.getElementById("UserName").innerHTML="ddddddddddddd";
    //document.document.querySelectorAll("#UserName").value="mossabbbb"
-   var  t =document.getElementById('UserName').value=UserName
-    var  t =document.getElementById('password-input').value=password
+   var  t =document.getElementById('UserName')
+   t.value="0436787";
+    var  p=document.getElementById('password-input')
+     p.value="Idhm-99-hga";
+   var submit =document.getElementById('submit_btn')
+   submit.click();
 })
-await page.getElementById("submit_btn").click;
-//await page.waitForNavigation(); 
+await page.waitForNavigation(); 
 
-    await page.screenshot({path: 'screenshot.png'});
+try{
+var message = await page.evaluate(() => {
+    return  document.getElementById('messages').innerHTML
+})
+console.log(message);
+    await page.screenshot({path: 'screenshot1.png'});
+}
+catch{
+    console.log("true login ")
+}
+
+if(typebill){
+await page.goto("https://www.bbsfonline.com/BbsfOnline/User/SEPServices/Index").then(()=>
+{
+   page.evaluate(()=>{
+    var select = document.getElementById('CategoryCode')
+    select.selectedIndex = 0
+})
+})
+
+
+
+//await page.select("select#", 'الاتصالات')
+
+await page.screenshot({path: 'screenshot2.png'});
+
+
+
+
+
+
+
+}
+else
+{
+    //electic bills
+}
+
+
+
     browser.close();
 }
 run();
