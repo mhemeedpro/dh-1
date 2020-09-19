2bc2edfc7ecd1d4e43ca3bf85c6fe5ff71cbcfac
mhemeedpro <mosabmhemed@gmail.com>
2020-09-19 14:58:03 +0300

    add new dir bots

--- test24.js
+++ test24.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const url = "https://www.bbsfonline.com/BbsfOnline/Public/User/Login"
+const UserName ="mosaabmosaab"
+const password="123456789"
 if (!url) {
     throw "Please provide URL as a first argument";
 }
@@ -8,9 +10,13 @@ async function run () {
     const page = await browser.newPage();
     await page.goto(url);
     await page.evaluate(() => {
-        document.getElementById("UserName").innerHTML="ddddddddddddd";
+      //  document.getElementById("UserName").innerHTML="ddddddddddddd";
    //document.document.querySelectorAll("#UserName").value="mossabbbb"
+   var  t =document.getElementById('UserName').value=UserName
+    var  t =document.getElementById('password-input').value=password
 })
+await page.getElementById("submit_btn").click;
+//await page.waitForNavigation(); 
 
     await page.screenshot({path: 'screenshot.png'});
     browser.close();
